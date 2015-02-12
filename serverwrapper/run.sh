b7c7b0f165ea45052dec1ddd01c4ab6e68e11515
James Harris <james@rancher.com>
2015-02-11 21:56:39 -0700

    Server logs

--- serverwrapper/run.sh
+++ serverwrapper/run.sh
@@ -8,10 +8,7 @@ ls
 cd ./Server
 docker build -t nodeserver .
 docker run  -d --net=host --name=nodeserver -p 8000:8000 -d nodeserver
-docker images
-count=0
 while sleep 10
 do
-    echo $count
-    count=$(($count + 1))
+    docker logs -f nodeserver
 done
