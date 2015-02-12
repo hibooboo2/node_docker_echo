a40d3d91b3b00d3894e6ad329a5654541270662c
James Harris <james@rancher.com>
2015-02-11 21:00:16 -0700

    Run as it

--- wrapper/start.sh
+++ wrapper/start.sh
@@ -4,9 +4,5 @@ wrapdocker
 cd /source/client/
 docker build -t client .
 docker rm -fv client
-docker run -d --net=host --name=client client
+docker run -it --rm --net=host -p 8000:8000 --name=client client
 #docker run -d -v /var/run/docker.sock:/var/run/docker.sock --name=client client
-
-while sleep 1; do
-    docker logs -f client
-done
