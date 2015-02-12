4cee1a00ed53d56e01a73c3e11d69386716ff9f7
James Harris <james@rancher.com>
2015-02-12 08:55:27 -0700

    Print to show is working.

--- serverwrapper/server/server.js
+++ serverwrapper/server/server.js
@@ -2,7 +2,7 @@
 
 var hostAddress;
 require('dns').lookup(require('os').hostname(), function (err, add, fam) {
-  hostAddress = add;
+    hostAddress = add;
 })
 
 var http = require('http');
@@ -10,6 +10,12 @@ http.createServer(function (req, res) {
     res.writeHead(200, {
         'Content-Type': 'text/plain'
     });
-    res.end('Hello from '+ hostAddress +'\n');
-    console.log("Ping from "+ req.url);
+    res.end('Hello' + req.url + '\n');
+    console.log("Ping from " + req.url);
 }).listen(8000);
+
+var count = 0;
+setInterval(function () {
+    console.log("Server working " + count);
+    count++;
+},1000)
