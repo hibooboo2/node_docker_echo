ccc06589403e66104fd20c37ffc0d430f41ba48c
James Harris <james@rancher.com>
2015-02-14 00:09:54 -0700

    Add net host

--- clientwrapper/start.sh
+++ clientwrapper/start.sh
@@ -5,7 +5,7 @@ cd /source/client/
 docker rm -fv $(docker ps -qa)
 docker build -t client .
 docker rm -fv client
-docker run -d --name=client client
+docker run -d --net=host --name=client client
 #docker run -d -v /var/run/docker.sock:/var/run/docker.sock --name=client client
 
 while sleep 10
